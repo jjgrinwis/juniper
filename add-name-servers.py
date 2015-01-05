03fa7b447b8dca42d86b0a32b6ca585d9692a20f
jjgrinwis <john@grinwis.com>
2015-01-05 08:11:27 +0100

    some error handling added

---
+++ add-name-servers.py
@@ -0,0 +1,61 @@
+# First PyEz test with YAML and jinja2 template engine
+# YAML file should contain juniper devices to configre
+
+import yaml
+from pprint import pprint
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import *
+
+# create datastructure from YAML file.
+# yaml.load takes a stream so open file object from yaml file.
+myvars = yaml.load(open('configuration.yml').read())
+
+# myvars has become a python dictionary thanx to YAML lib.
+pprint(myvars)
+
+# now create juniper device object from PyEZ Device class and open this device.
+for juniper in myvars['juniper_devices']:
+    print "Configuring %s" % juniper
+    device = Device(host=juniper, user='root', password='password123')
+
+    try:
+        # Now try to open connection to device
+        output = device.open()
+
+    # bind err variable to Exception raised by PyEZ open() method from Device class
+    # if anything wrong, an inherited class from jnpr.junos.exception.ConnectError base class raised
+    except Exception as err:
+        # print type(err)
+        print "Can't connect to %s, ERROR: %s" % (err.host, err)
+        exit(1)
+
+    # now bind Config class to this device which makes it a property of the :class:Device instance
+    device.bind(cfg=Config)
+
+    # load can take our jinja2 template with our myvars from YAML file
+    # The following parameters are needed for the load command:
+    # 1: template_path (str): path to jinja2 template file
+    # 2: template_vars (dict): dictionary build from YAML file, same vars are in jinja2 template
+    # 3: set format to text, otherwise you will see errors
+    device.cfg.load(template_path='config-template.j2', template_vars=myvars, format='text')
+
+    # print configuration differences
+    device.cfg.pdiff()
+
+    # now check configuration and if it's all OK, just commit.
+    # otherwise raise specific error. commit_check should return jnpr.junos.exception.RpcError class
+    try:
+        commit_check = device.cfg.commit_check()
+    except Exception as err:
+        print type(err)
+        print err.rsp
+        exit(1)
+
+    # now commit config and close connection to juniper device
+    if commit_check is True:
+        device.cfg.commit()
+
+    device.close()
+
+print "configuration of devices done"
